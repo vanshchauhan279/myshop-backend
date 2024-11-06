ba6b7ca2fe5b907ef41bc2cdf4919847fca5f13f
vansh <chauhanvansh279@gmail.com>
2024-11-06 21:24:45 +0530

    changes in productRouter

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -3,34 +3,31 @@ const productRouter = express.Router();
 const fs = require("fs");
 const csv = require("csv-parser");
 const Product = require("../models/productSchema");
-const path = require('path');
+const path = require("path");
 
 
+// we have to write middleware for route restricted to developer of company
 productRouter.post("/body", async (req, res) => {
-
-  const csvPath = path.resolve(__dirname, '../utils/fashionDataset.csv');
-  
   try {
+    const csvPath = path.resolve(__dirname, "../utils/fashionDataset.csv");
     const products = [];
     fs.createReadStream(csvPath)
       .pipe(csv())
       .on("data", (row) => {
         products.push(row);
       })
-      .on("end", async() => {
-        try{
-           await Product.insertMany(products)
-           res.send("Data Imported Successfully")
+      .on("end", async () => {
+        try {
+         const mainObj = {products: products}
+          await Product.insertMany(mainObj.products);
+          res.send("Data Imported Successfully");
+        } catch (err) {
+          res.status(404).send(err);
         }
-        catch(err){
-            res.status(404).send(err);
-        }
-       
       });
   } catch (err) {
-       res.status(404).send(err);
+    res.status(404).send(err);
   }
 });
 
 module.exports = productRouter;
-  
