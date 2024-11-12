78e4394d4a659b84dea9466b6971d5b419901561
vansh <chauhanvansh279@gmail.com>
2024-11-13 04:14:15 +0530

    product logic in add to cart router

--- src/routes/cartRouter.js
+++ src/routes/cartRouter.js
@@ -1,3 +1,21 @@
 const express = require("express");
+const { findById } = require("../models/cartSchema");
+const Product = require("../models/productSchema");
 const cartRouter = express.Router();
+
+cartRouter.post('/addtoCart/:id',async(req,res)=>{
+    try{
+          const {id} = req.params;
+          const product = await Product.findById(id);
+          if(!product){
+            return res.status(500).send("Please add valid Product in the Cart");
+          }
+          
+          //user ref to logged in user
+
+    }
+    catch(err){
+        res.status(404).send("Add to Cart");
+    }
+})
 
