0646700f03973e75dec90cce38037f3cb6f3668a
vansh <chauhanvansh279@gmail.com>
2024-11-15 19:03:36 +0530

    remove item from cart

--- src/routes/cartRouter.js
+++ src/routes/cartRouter.js
@@ -12,13 +12,13 @@ cartRouter.post("/addtoCart/:id", profileAuth, async (req, res) => {
 
     const user = await User.findById(userId);
     if (!user) {
-      return res.status(500).send("Logged In Failed");
+      return res.status(404).send("Logged In Failed");
     }
 
     const product = await Product.findById(id);
 
     if (!product) {
-      return res.status(500).send("Please add valid Product in the Cart");
+      return res.status(404).send("Please add valid Product in the Cart");
     }
 
     const isProductInCart = await Cart.findOne({
@@ -26,7 +26,7 @@ cartRouter.post("/addtoCart/:id", profileAuth, async (req, res) => {
       user: user._id,
     });
     if (isProductInCart) {
-      return res.status(500).send("Product is already present");
+      return res.status(404).send("Product is already present");
     }
 
     const cart = await Cart.create({
@@ -35,19 +35,36 @@ cartRouter.post("/addtoCart/:id", profileAuth, async (req, res) => {
     });
     res.send(cart);
   } catch (err) {
-    res.status(404).send(err);
+    res.status(400).send(err);
   }
 });
 
-cartRouter.get("/cart",profileAuth,async(req,res)=>{
-  try{
-        const userId = req.user._id;
-        const cartItems= await Cart.find({user: userId});
-        res.send(cartItems)
-  }
-  catch(err){
+cartRouter.get("/cart", profileAuth, async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const cartItems = await Cart.find({ user: userId });
+    res.send(cartItems);
+  } catch (err) {
     res.status(400).send(err);
   }
-})
+});
+
+cartRouter.delete("/removeFromCart/:id", profileAuth, async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const { id } = req.params;
+    const cartItem = await Cart.findOne({ user: userId, _id: id });
+
+    if (!cartItem) {
+      return res.status(404).send("Item is not present is cart");
+    }
+
+    const newCart = await Cart.deleteOne(cartItem);
+
+    res.send("Succesfully deleted from cart");
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
 module.exports = cartRouter;
